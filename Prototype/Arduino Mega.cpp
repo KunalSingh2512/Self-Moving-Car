e0bf0b9e2a7f2bf75ffcdb78bd8b2fd047d709b1
Kunal Singh <singhk122007@gmail.com>
2026-01-07 12:39:21 +0530

    U-turn included

--- Prototype/Arduino Mega.cpp
+++ Prototype/Arduino Mega.cpp
@@ -1,35 +1,49 @@
 const int LPWM_L = 5;
-const int RPWM_L = 6;   
+const int RPWM_L = 6;
 const int LPWM_R = 9;
 const int RPWM_R = 10;
 
-const unsigned long forwardTime  = 2300;
-const unsigned long backwardTime = 2300;
-const int motorSpeed = 200;
+const unsigned long travelTime = 2000;
+const int motorSpeed = 220;
+const int pulseCount = 6;
 
 void setup() {
   pinMode(LPWM_L, OUTPUT);
   pinMode(RPWM_L, OUTPUT);
   pinMode(LPWM_R, OUTPUT);
   pinMode(RPWM_R, OUTPUT);
-
+  
   stopMotors();
 }
 
 void loop() {
   driveForward(motorSpeed);
-  delay(forwardTime);
+  delay(travelTime);
+
+  executePulsingUTurn();
+
+  driveForward(motorSpeed); 
+  delay(travelTime);
+
+  executePulsingUTurn();
 
   stopMotors();
-  delay(300);
-
-  driveBackward(motorSpeed);
-  delay(backwardTime);
-
-  stopMotors();
-  
   while (true) {
     stopMotors();
+  }
+}
+
+
+void executePulsingUTurn() {
+  for (int i = 0; i < pulseCount; i++) {
+    analogWrite(LPWM_L, 0);
+    analogWrite(RPWM_L, motorSpeed); 
+    analogWrite(LPWM_R, motorSpeed); 
+    analogWrite(RPWM_R, 0);
+    delay(450); 
+
+    driveForward(motorSpeed); 
+    delay(250); 
   }
 }
 
@@ -38,13 +52,6 @@ void driveForward(int speed) {
   analogWrite(RPWM_L, 0);
   analogWrite(LPWM_R, speed);
   analogWrite(RPWM_R, 0);
-}
-
-void driveBackward(int speed) {
-  analogWrite(LPWM_L, 0);
-  analogWrite(RPWM_L, speed);
-  analogWrite(LPWM_R, 0);
-  analogWrite(RPWM_R, speed);
 }
 
 void stopMotors() {
