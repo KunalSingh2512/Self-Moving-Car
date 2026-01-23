a411abbd3cfd0a9a41ea2135c7c253952adbb23c
Kunal Singh <singhk122007@gmail.com>
2026-01-23 20:42:48 +0530

    Ready to test

--- ros2_ws/install/circular_drive_controller/share/circular_drive_controller/package.zsh
+++ ros2_ws/install/circular_drive_controller/share/circular_drive_controller/package.zsh
@@ -1,8 +1,5 @@
 # generated from colcon_zsh/shell/template/package.zsh.em
 
-# This script extends the environment for this package.
-
-# a zsh script is able to determine its own path if necessary
 if [ -z "$COLCON_CURRENT_PREFIX" ]; then
   # the prefix is two levels up from the package specific share directory
   _colcon_package_zsh_COLCON_CURRENT_PREFIX="$(builtin cd -q "`dirname "${(%):-%N}"`/../.." > /dev/null && pwd)"
@@ -10,9 +7,6 @@ else
   _colcon_package_zsh_COLCON_CURRENT_PREFIX="$COLCON_CURRENT_PREFIX"
 fi
 
-# function to source another script with conditional trace output
-# first argument: the path of the script
-# additional arguments: arguments to the script
 _colcon_package_zsh_source_script() {
   if [ -f "$1" ]; then
     if [ -n "$COLCON_TRACE" ]; then
@@ -24,8 +18,6 @@ _colcon_package_zsh_source_script() {
   fi
 }
 
-# function to convert array-like strings into arrays
-# to workaround SH_WORD_SPLIT not being set
 colcon_zsh_convert_to_array() {
   local _listname=$1
   local _dollar="$"
@@ -34,7 +26,6 @@ colcon_zsh_convert_to_array() {
   eval $_listname=$_to_array
 }
 
-# source sh script of this package
 _colcon_package_zsh_source_script "$_colcon_package_zsh_COLCON_CURRENT_PREFIX/share/circular_drive_controller/package.sh"
 unset convert_zsh_to_array
 
