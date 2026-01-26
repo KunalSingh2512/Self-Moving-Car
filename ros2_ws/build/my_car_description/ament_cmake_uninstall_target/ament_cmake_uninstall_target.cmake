b1a2d81aa691a1a6cab90daba535c03415841b91
Kunal Singh <singhk122007@gmail.com>
2026-01-26 17:49:22 +0530

    System Architure Ready for 27th Plan

--- ros2_ws/build/my_car_description/ament_cmake_uninstall_target/ament_cmake_uninstall_target.cmake
+++ ros2_ws/build/my_car_description/ament_cmake_uninstall_target/ament_cmake_uninstall_target.cmake
@@ -55,3 +55,6 @@ endforeach()
 message(STATUS "Execute custom uninstall script")
 
 # begin of custom uninstall code
+
+# uninstall files installed using the symlink install functions
+include("/home/kunal-singh/Desktop/Self_Moving_Car/ros2_ws/build/my_car_description/ament_cmake_symlink_install/ament_cmake_symlink_install_uninstall_script.cmake")
