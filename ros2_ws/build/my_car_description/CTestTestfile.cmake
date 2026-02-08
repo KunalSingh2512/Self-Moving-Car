542cda27ba20dd38e227a6cc215ceba75fbb5afb
Kunal Singh <singhk122007@gmail.com>
2026-02-08 23:29:06 +0530

    Fixed repository structure: Added src, removed build artifacts

--- ros2_ws/build/my_car_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kunal-singh/Desktop/Self_Moving_Car/ros2_ws/src/my_car_description
-# Build directory: /home/kunal-singh/Desktop/Self_Moving_Car/ros2_ws/build/my_car_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
