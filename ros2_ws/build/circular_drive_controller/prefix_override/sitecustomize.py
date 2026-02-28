8242279db1246ffe4ca673afca4255a694f62785
Kunal Singh <singhk122007@gmail.com>
2026-02-28 17:18:00 +0530

    Circular Drive Advancement 1(CDA)

--- ros2_ws/build/circular_drive_controller/prefix_override/sitecustomize.py
+++ ros2_ws/build/circular_drive_controller/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/kunal-singh/Desktop/Self_Moving_Car/ros2_ws/install/circular_drive_controller'
+    sys.prefix = sys.exec_prefix = '/home/kunal-singh/Desktop/Self-Moving-Car/ros2_ws/install/circular_drive_controller'
